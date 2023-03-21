baf6902ed42add4e220dae331eba8ed96dc9b9a1
Admin <Admin@192.168.1.100>
2023-03-21 11:23:10 +0000

    creating VehicleNotFoundException

---
+++ src/main/java/com/qa/garage/vehicle/VehicleNotFoundException.java
@@ -0,0 +1,18 @@
+package com.qa.garage.vehicle;
+
+public class VehicleNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2586551681093346112L;
+	
+	public VehicleNotFoundException() {
+		super();
+	}
+	
+	public VehicleNotFoundException(String message) {
+		super(message);
+	}
+
+}
